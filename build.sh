cb3b37513859bea61b587a55938e0d5683f23cbb
Steve Phillips <steve@crownedwarrior.com>
2026-01-22 14:54:37 -0500

    Test code

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+gcc ./test01.c -o test01
+
--- test01.c
+++ test01.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) 
+{
+    if(argc != 2)
+    {
+        printf("usage: test01 <number>\n");
+    }
+    else
+    {
+        printf("Counting down...\n");
+
+        for(int ii = atoi(argv[1]); ii > 0; ii--)
+        {
+            printf("%d \r", ii);
+            fflush(stdout);
+            sleep(1);
+        }
+        printf("done!\n");
+    }
+
+    return 0;
+}
